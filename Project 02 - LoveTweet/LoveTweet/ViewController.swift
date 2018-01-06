ba1ac8bbc881c8b5b80ca2c8b04443daeaea19d6
teaocat <teaocat@gmail.com>
2018-01-06 21:06:30 +0800

    Add Project 02 - LoveTweet
    Add Project 02 - LoveTweet, and complete Initialize

---
+++ Project 02 - LoveTweet/LoveTweet/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  LoveTweet
+//
+//  Created by Teaocat on 2018/1/6.
+//  Copyright © 2018年 school. All rights reserved.
+//
+//  Github: https://github.com/teaocat
+//
+    
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
