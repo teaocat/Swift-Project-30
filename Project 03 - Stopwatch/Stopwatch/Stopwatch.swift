44955d36c0f1c2340aef23488dbd022df15bae5b
teaocat <teaocat@gmail.com>
2018-01-09 11:10:13 +0800

    Add Stopwatch.swift file
    Add Stopwatch.swift file

---
+++ Project 03 - Stopwatch/Stopwatch/Stopwatch.swift
@@ -0,0 +1,23 @@
+//
+//  Stopwatch.swift
+//  Stopwatch
+//
+//  Created by Teaocat on 2018/1/9.
+//  Copyright © 2018年 school. All rights reserved.
+//
+//  Github: https://github.com/teaocat
+//
+    
+
+import UIKit
+
+class Stopwatch: NSObject {
+    var count: Double
+    var timer: Timer
+    
+    override init() {
+        count = 0.0
+        timer = Timer()
+    }
+    
+}
