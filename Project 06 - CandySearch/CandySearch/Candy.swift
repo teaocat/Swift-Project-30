0fcf06c1bbc2f38c1de7a70dc6ed932d77e72e0d
teaocat <teaocat@gmail.com>
2018-01-12 12:03:29 +0800

    Update Project 06
    Complete UI Layout

---
+++ Project 06 - CandySearch/CandySearch/Candy.swift
@@ -0,0 +1,22 @@
+//
+//  Candy.swift
+//  CandySearch
+//
+//  Created by Teaocat on 2018/1/12.
+//  Copyright © 2018年 school. All rights reserved.
+//
+//  Github: https://github.com/teaocat
+//
+    
+
+import Foundation
+
+class Candy {
+    var category: String
+    var name: String
+    
+    init(category: String, name: String) {
+        self.category = category
+        self.name = name
+    }
+}
---
+++ Project 06 - CandySearch/CandySearch/DetailViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DetailViewController.swift
+//  CandySearch
+//
+//  Created by Teaocat on 2018/1/12.
+//  Copyright © 2018年 school. All rights reserved.
+//
+//  Github: https://github.com/teaocat
+//
+    
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
