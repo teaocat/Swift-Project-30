1682313f7ebeb56f09ea613d2726ddffdb90f50f
teaocat <teaocat@gmail.com>
2018-03-02 13:34:48 +0800

    Initialization Project 08
    Initialization Project 08

---
+++ Project 08 - SimpleRSSReader/SimpleRSSReader/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  SimpleRSSReader
+//
+//  Created by Teaocat on 2018/3/2.
+//  Copyright © 2018年 learning-swift. All rights reserved.
+//
+//  Github: https://github.com/teaocat
+//
+    
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
